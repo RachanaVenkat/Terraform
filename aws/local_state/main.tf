758a721a991b0c03162ef8c54efea79990a1ae78
“Rachana” <rachanaa.v.01@gmail.com>
2024-03-08 10:22:21 +0530

    basic terraform script to create aws ec2 instance

---
+++ aws/local_state/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
